8cbc3a26cc62ff4fbfb6310b87e85c1687020700
peter2500zz <2797880090@qq.com>
2026-05-10 14:20:27 +0800

    合并测试项

--- tests/test_jump.c
+++ tests/test_jump.c
@@ -3,6 +3,17 @@
 int main(void) {
         SETUP_VM(4);
 
+        // JAL
+
+        vm->_pc = 0;
+
+        // jal x1, 8
+        vm_exec(vm, 0x008000ef);
+        ASSERT_EQ(vm->_pc, 8);
+        ASSERT_EQ(vm->_regs[1], 4);
+
+        // JALR
+
         vm->_pc = 0;
         vm->_regs[1] = 4;
 
