6d9230670fb062269d7c3bc4b9bfe59998ea0c57
peter2500zz <2797880090@qq.com>
2026-06-26 13:40:15 +0800

    chore: 添加注释

--- src/utils/mmioMap.c
+++ src/utils/mmioMap.c
@@ -20,12 +20,13 @@ MmioMap *newMmioMap(void) {
         if (mmioMap == NULL) {
                 goto out;
         }
+        // 初始化
         mmioMap->capacity = 256;
         mmioMap->buckets = calloc(mmioMap->capacity, sizeof(*mmioMap->buckets));
-        mmioMap->nodeCount = 0;
         if (mmioMap->buckets == NULL) {
                 goto out_free_mmiomap;
         }
+        mmioMap->nodeCount = 0;
 
         return mmioMap;
 
@@ -35,18 +36,28 @@ out:
         return NULL;
 }
 
-void freeMmioMap(MmioMap *mmioMap) {
-        if (mmioMap == NULL) {
+void freeMmioMap(MmioMap **pMmioMap) {
+        // 检查指针是否为 NULL
+        if (pMmioMap == NULL) {
                 goto out;
         }
+        MmioMap *mmioMap = *pMmioMap;
 
-        if (mmioMap->buckets == NULL) {
-                goto out_free_mmiomap;
+        // 检查 MmioMap 是否是 NULL
+        if (mmioMap == NULL) {
+                goto out_clear;
         }
 
+        // 检查桶是否为空
+        if (mmioMap->buckets == NULL) {
+                goto out_free_map;
+        }
+
+        // 遍历释放桶
         for (uint32_t i = 0; i < mmioMap->capacity; i++) {
                 MmioNode *node = mmioMap->buckets[i];
 
+                // 遍历释放节点
                 while (node != NULL) {
                         MmioNode *next = node->next;
                         free(node);
@@ -55,8 +66,10 @@ void freeMmioMap(MmioMap *mmioMap) {
         }
         free(mmioMap->buckets);
 
-out_free_mmiomap:
+out_free_map:
         free(mmioMap);
+out_clear:
+        *pMmioMap = NULL;
 out:;
 }
 
@@ -65,17 +78,22 @@ int extendMmioMap(MmioMap *mmioMap) {
                 return 1;
         }
 
+        // 保存当前 capacity 和桶
         const uint32_t oldCapacity = mmioMap->capacity;
         MmioNode **oldBuckets = mmioMap->buckets;
 
+        // 创建新桶
         const uint32_t newCapacity = oldCapacity * 2;
         MmioNode **newBuckets = calloc(newCapacity, sizeof(*newBuckets));
         if (newBuckets == NULL) {
                 return 1;
         }
 
+        // 遍历旧桶
         for (uint32_t i = 0; i < oldCapacity; i++) {
                 MmioNode *node = oldBuckets[i];
+
+                // 遍历节点，rehash 并迁移内容
                 while (node != NULL) {
                         MmioNode *next = node->next;
 
@@ -88,6 +106,7 @@ int extendMmioMap(MmioMap *mmioMap) {
                 }
         }
 
+        // 释放旧桶
         free(oldBuckets);
         mmioMap->buckets = newBuckets;
         mmioMap->capacity = newCapacity;
@@ -100,10 +119,13 @@ int putMmioMap(MmioMap *mmioMap, const uint32_t address, const accessFunc read,
                 return 1;
         }
 
+        // 计算 hash 并找到桶
         const uint32_t index = hash_u32(address) & (mmioMap->capacity - 1);
         MmioNode *node = mmioMap->buckets[index];
 
+        // 遍历节点判断是否有重复地址
         while (node != NULL) {
+                // 重复地址直接替换并返回
                 if (node->address == address) {
                         node->func.read = read;
                         node->func.write = write;
@@ -112,22 +134,27 @@ int putMmioMap(MmioMap *mmioMap, const uint32_t address, const accessFunc read,
                 node = node->next;
         }
 
+        // 分配新节点
         MmioNode *newNode = malloc(sizeof(MmioNode));
         if (newNode == NULL) {
                 return 1;
         }
+        // 初始化新节点
         newNode->address = address;
         newNode->func.read = read;
         newNode->func.write = write;
 
+        // 插入新节点
         newNode->next = mmioMap->buckets[index];
         mmioMap->buckets[index] = newNode;
         mmioMap->nodeCount++;
 
+        // 计算负载因子
         const float loadFactor =
             (float)mmioMap->nodeCount / (float)mmioMap->capacity;
 
         if (loadFactor > 0.75f) {
+                // 扩容桶
                 if (extendMmioMap(mmioMap)) {
                         // Failed to extend, what is happening?
                 }
@@ -144,9 +171,11 @@ static accessFunc getMmioMap(const MmioMap *mmioMap, const uint32_t address,
                 return func;
         }
 
+        // 计算 hash 并获取桶
         const uint32_t index = hash_u32(address) & (mmioMap->capacity - 1);
         const MmioNode *node = mmioMap->buckets[index];
 
+        // 遍历节点寻找值
         while (node != NULL) {
                 if (node->address == address) {
                         if (isReadFunc) {
--- src/utils/mmioMap.h
+++ src/utils/mmioMap.h
@@ -6,8 +6,18 @@
 #define VM_RISCV_MMIOMAP_H
 #include <stdint.h>
 
+/**
+ * @brief MMIO 操作函数定义
+ *
+ * 对于读函数，读取到的值将会被写入传入指针中
+ *
+ * 对于写函数，传入指针中的值将会被写入目标
+ */
 typedef int (*accessFunc)(void *);
 
+/**
+ * @brief MMIO 地址单元
+ */
 typedef struct MmioNode {
         uint32_t address;
         struct {
@@ -17,22 +27,70 @@ typedef struct MmioNode {
         struct MmioNode *next;
 } MmioNode;
 
+/**
+ * @brief MMIO 映射表，通过地址作为键来获取对应函数
+ *
+ * @note 不同宽度的操作应当使用多个映射表
+ */
 typedef struct MmioMap {
         uint32_t capacity;
         int nodeCount;
         MmioNode **buckets;
 } MmioMap;
 
+/**
+ * @brief 创建 MmioMap
+ *
+ * @return 如果内存分配成功，返回 MmioMap *
+ *
+ * 否则，返回 NULL
+ */
 MmioMap *newMmioMap(void);
 
-void freeMmioMap(MmioMap *mmioMap);
+/**
+ * @brief 释放一个 MmioMap 和其中所有的值，并将指针置为 NULL
+ *
+ * @param pMmioMap 指向 MmioMap 实例的指针
+ */
+void freeMmioMap(MmioMap **pMmioMap);
 
+/**
+ * @brief 将一个 MmioMap 扩容到当前的两倍大小
+ *
+ * @param mmioMap MmioMap 实例
+ * @return 非 0 为扩容失败
+ */
 int extendMmioMap(MmioMap *mmioMap);
 
+/**
+ * @brief 向 MmioMap 中添加读写函数
+ *
+ * @param mmioMap MmioMap 实例
+ * @param address MMIO 地址
+ * @param read 读函数
+ * @param write 写函数
+ * @return 非 0 为添加失败
+ *
+ * @note 如果地址已存在，将覆盖原先值
+ */
 int putMmioMap(MmioMap *mmioMap, uint32_t address, accessFunc read, accessFunc write);
 
+/**
+ * @brief 获取读函数
+ *
+ * @param mmioMap MmioMap 实例
+ * @param address MMIO 地址
+ * @return 读函数，不存在或获取失败时为 NULL
+ */
 accessFunc getReadFuncMmioMap(const MmioMap *mmioMap, uint32_t address);
 
+/**
+ * @brief 获取写函数
+ *
+ * @param mmioMap MmioMap 实例
+ * @param address MMIO 地址
+ * @return 读函数，不存在或获取失败时为 NULL
+ */
 accessFunc getWriteFuncMmioMap(const MmioMap *mmioMap, uint32_t address);
 
 #endif // VM_RISCV_MMIOMAP_H
